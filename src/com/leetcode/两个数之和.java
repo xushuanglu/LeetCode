ff837128e336c636989dd89d16f15dc4b777ff13
Administrator <Administrator@SD-20181229CKLG>
2019-02-25 17:57:48 +0800

    代码优化

--- src/com/leetcode/两个数之和.java
+++ src/com/leetcode/两个数之和.java
@@ -48,18 +48,38 @@ public class 两个数之和 {
 			//临时值  target=9  nums[i] = 2  0201
 			int temp = target - nums[i];
 			if(map.containsKey(temp) && map.get(temp) != i) {
-				System.out.println(i);
-				System.out.println(map.get(temp));
 				return new int[] {i,map.get(temp)};
 			}
 		}
-		return null;
+		throw new IllegalArgumentException("No two sum solution");
+	}
+	
+	/**
+	 * 方法三：一遍哈希表
+                 事实证明，我们可以一次完成。在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。如果它存在，那我们已经找到了对应解，并立即将其返回。
+                 
+                 复杂度分析：
+			时间复杂度：
+			O(n),我们只遍历了包含有 n 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
+			空间复杂度：O(n)， 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 n 个元素。
+		
+	 * @param args
+	 */
+	public int[] twoSum1(int[] nums, int target) {
+	    Map<Integer, Integer> map = new HashMap<>();
+	    for (int i = 0; i < nums.length; i++) {
+	        int complement = target - nums[i];
+	        if (map.containsKey(complement)) {
+	            return new int[] { map.get(complement), i };
+	        }
+	        map.put(nums[i], i);
+	    }
+	    throw new IllegalArgumentException("No two sum solution");
 	}
 	
 	
 	public static void main(String[] args) {
 		int[] nums = {2, 7, 11, 15};
-//		twoSum(nums,9);
 		twoSumOptimize(nums,9);
 	}
 }
